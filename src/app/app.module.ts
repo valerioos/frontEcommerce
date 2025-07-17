5002876b8a4f324204cd0ef02368aa2540e65aa4
anayeli <anayelivaleriovelez0@gmail.com>
2025-07-17 14:23:54 -0600

    se añadio servicio producto, se realizo nabvar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,11 @@ import { PedidosComponent } from './components/pedidos/pedidos.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { ErrorInterceptor } from './shared/error.interceptor';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,13 +21,18 @@ import { NavbarComponent } from './components/common/navbar/navbar.component';
     PedidosComponent,
     ClientesComponent,
     PrincipalComponent,
-    NavbarComponent
+    NavbarComponent,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { enviroment } from '../environments/environmet';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
+import { ProductoResponse } from '../models/productosResponse.model';
+import { ProductoRequest } from '../models/ProductosRequest.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  private apiUrl: string = enviroment.apiUrl + 'productos/';
+
+  constructor(private http: HttpClient) { }
+
+  getProductos(): Observable<ProductoResponse[]> {
+    return this.http.get<ProductoResponse[]>(this.apiUrl).pipe(
+      map(productos => productos.sort()),
+      catchError(error =>{
+        console.log('error al obtener los productos', error);
+        return of([]);
+      })
+    );
+  }
+
+  postProducto(producto: ProductoRequest): Observable<ProductoResponse> {
+    return this.http.post<ProductoResponse>(this.apiUrl, producto).pipe(
+      catchError(error => {
+        console.log('Error al insertar Producto', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  
+  putProducto(producto: ProductoRequest, productoId: number): Observable<ProductoResponse> {
+    return this.http.put<ProductoResponse>(`${this.apiUrl}${productoId}`, producto).pipe(
+      catchError(error => {
+        console.log('Error al actualizar Producto', error);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  deleteProducto(productoId: number): Observable<ProductoResponse> {
+    return this.http.delete<ProductoResponse>(`${this.apiUrl}${productoId}`).pipe(
+      catchError(error => {
+        console.log('Error al eliminar Producto', error);
+        return throwError(() => error);
+      })
+    );
+  }
 }
